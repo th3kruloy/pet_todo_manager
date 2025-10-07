ce0bf0bcc93ec202d8a821996d851a355493f2e5
th3kruloy <163294861+th3kruloy@users.noreply.github.com>
2025-10-07 16:55:15 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,20 @@
 #include <print>
 #include <conio.h>
 #include <limits>
+#include <nlohmann/json.hpp>
+#include <fstream>
 #include "task.h"
 
+using json = nlohmann::json;
+
 bool enumerate_tasks();
+bool save();
+bool load();
+
 std::vector<task*> tasks;
 
 int main() {
+	load();
 	while (true) {
 		system("cls");
 		std::string input;
@@ -40,6 +48,7 @@ int main() {
 			std::print("Write your task: ");
 			std::getline(std::cin >> std::ws, tmp_task->task_name);
 			tasks.push_back(tmp_task);
+			save();
 			break;
 		}
 		case 2:{
@@ -47,7 +56,7 @@ int main() {
 			break;
 		}
 		case 3: {
-			int selected_task = 0;
+			int selected_task_position = 0;
 			int selected_parameter = 0;
 			if (enumerate_tasks()) {
 				system("pause");
@@ -55,30 +64,52 @@ int main() {
 			}
 			while (true) {
 				std::println("Select your task by number: ");
-				std::cin >> selected_task;
-				if (selected_task <= 0 || selected_task > tasks.size()) { std::println("Wrong value: Try again"); }
+				std::cin >> selected_task_position;
+				if (selected_task_position <= 0 || selected_task_position > tasks.size()) { 
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::println("Wrong value: Try again");
+				}
 				else { break; }
 			}
-			size_t selected_task_vector = selected_task - 1;
+			auto* selected_task = tasks.at(selected_task_position - 1);
 			std::println("What parameter you want to change? (1 - Name, 2 - Completion Status): ");
 			std::cin >> selected_parameter;
 			bool success = true;
+			system("cls");
 			switch (selected_parameter)
 			{
 			case 1: {
 				std::println("Enter new task name: ");
-				std::getline(std::cin >> std::ws, tasks.at(selected_task_vector)->task_name);
+				std::getline(std::cin >> std::ws, selected_task->task_name);
 				break;
 			}
 			case 2: {
-				std::println("Enter new completion status (1 - true, 0 - false): ");
-				std::cin >> tasks.at(selected_task_vector)->status;
+				bool success = false;
+				while (!success) {
+					bool status;
+					std::println("Enter new completion status (true, or false): ");
+					std::cin >> std::boolalpha >> status;
+					if (std::cin.fail()) {
+						system("cls");
+						std::cin.clear();
+						std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+						std::println("Enter a valid value, and try again");
+						system("pause");
+						continue;
+					}
+					selected_task->status = status;
+					success = true;
+				}
 				break;
 			}
 			default:
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 				std::println("invalid");
 				break;
 			}
+			save();
 			break;
 		}
 		case 4: {
@@ -93,6 +124,7 @@ int main() {
 			tasks.erase(tasks.begin() + (selected_task - 1));
 			system("cls");
 			std::println("Done!");
+			save();
 			break;
 		}
 		case 5:
@@ -102,6 +134,7 @@ int main() {
 			break;
 		}
 		system("pause");
+		std::cin.clear();
 	}
 }
 
@@ -118,4 +151,54 @@ bool enumerate_tasks() {
 			" Status: " << std::boolalpha << tasks.at(_S)->status << std::endl;
 	}
 	return false;
+}
+
+bool save() {
+	json _J;
+	std::string filename = "tasks.json";
+	for (size_t _S = 0; _S < tasks.size(); _S++) {
+		task* current = tasks.at(_S);
+		_J[std::format("task{}", _S)]["name"] = current->task_name;
+		_J[std::format("task{}", _S)]["status"] = current->status;
+	}
+	try {
+		std::ofstream file(filename);
+		if (!file.is_open()) {
+			return false;
+		}
+
+		file << _J.dump(4);
+		file.close();
+
+		return true;
+	}
+	catch (const std::exception& e) {
+		return false;
+	}
+}
+
+bool load() {
+	std::string filename = "tasks.json";
+	try {
+		std::ifstream file(filename);
+		if (!file.is_open()) {
+			return false;
+		}
+
+		json _J;
+		file >> _J;
+		file.close();
+
+		for (size_t _S = 0; _S < _J.size(); _S++) {
+			task* tmp_task = new task;
+			tmp_task->task_name = _J[std::format("task{}", _S)]["name"];
+			tmp_task->status = _J[std::format("task{}", _S)]["status"];
+			tasks.push_back(tmp_task);
+		}
+
+		return true;
+	}
+	catch (const std::exception& e) {
+		return false;
+	}
 }
